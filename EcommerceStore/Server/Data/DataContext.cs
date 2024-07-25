6e65ba77e47758958be3a7ab1e069cd52c03123c
Anas khan <anaskhanomg@gmail.com>
2024-07-25 17:19:36 +0530

    Adds seeding data.

--- EcommerceStore/Server/Data/DataContext.cs
+++ EcommerceStore/Server/Data/DataContext.cs
@@ -13,5 +13,46 @@ namespace EcommerceStore.Server.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                    new Category { Id = 4, Name = "books", Url="books", Icon="book" },
+                    new Category { Id = 2, Name = "glasses", Url = "glasses", Icon = "book" },
+                    new Category { Id = 3, Name = "bikes", Url = "bikes", Icon = "book" }
+                );
+            modelBuilder.Entity<Product>().HasData(
+                 new Product
+                 {
+                     Id = 1,
+                     CategoryId = 2,
+                     Title = "first product",
+                     Description = "this is some amazing product",
+                     Price = (decimal)9.99,
+                     OriginalPrice = (decimal)10.99,
+                     Image = "urls",
+                 },
+                new Product
+                {
+                    Id = 2,
+                    CategoryId = 3,
+                    Title = "second product",
+                    Description = "this is some amazing product",
+                    Price = (decimal)9.99,
+                    OriginalPrice = (decimal)10.99,
+                    Image = "urls",
+                },
+                new Product
+                {
+                    Id = 3,
+                    CategoryId = 4,
+                    Title = "third product",
+                    Description = "this is some amazing product",
+                    Price = (decimal)9.99,
+                    OriginalPrice = (decimal)10.99,
+                    Image = "urls",
+                }
+                );
+        }
     }
 }
---
+++ EcommerceStore/Server/Migrations/20240725105648_seedingdata.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceStore.Server.Migrations
+{
+    public partial class seedingdata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Icon", "Name", "Url" },
+                values: new object[] { 2, "book", "glasses", "glasses" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Icon", "Name", "Url" },
+                values: new object[] { 3, "book", "bikes", "bikes" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Icon", "Name", "Url" },
+                values: new object[] { 4, "book", "books", "books" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "DateCreated", "DateUpdated", "Description", "Image", "IsDeleted", "IsPublic", "OriginalPrice", "Price", "Title" },
+                values: new object[] { 1, 2, new DateTime(2024, 7, 25, 16, 26, 47, 958, DateTimeKind.Local).AddTicks(405), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "this is some amazing product", "urls", false, false, 10.99m, 9.99m, "first product" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "DateCreated", "DateUpdated", "Description", "Image", "IsDeleted", "IsPublic", "OriginalPrice", "Price", "Title" },
+                values: new object[] { 2, 3, new DateTime(2024, 7, 25, 16, 26, 47, 958, DateTimeKind.Local).AddTicks(416), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "this is some amazing product", "urls", false, false, 10.99m, 9.99m, "second product" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "DateCreated", "DateUpdated", "Description", "Image", "IsDeleted", "IsPublic", "OriginalPrice", "Price", "Title" },
+                values: new object[] { 3, 4, new DateTime(2024, 7, 25, 16, 26, 47, 958, DateTimeKind.Local).AddTicks(417), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "this is some amazing product", "urls", false, false, 10.99m, 9.99m, "third product" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
